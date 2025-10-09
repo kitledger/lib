0d662bddeb43ae1f7123bce88cd12f1b5e8d415e
Alejandro Barrera Aponte <alebarrera76@gmail.com>
2025-10-09 18:07:41 -0300

    Improved Query Terminology

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -1,65 +1,65 @@
 import * as v from 'valibot';
 
 // --- Base Filter Schemas ---
-const BaseFilterSchema = v.object({
-    field: v.string(),
+const BaseConditionSchema = v.object({
+    column: v.string(),
 });
 
-const ArrayValueFilterSchema = v.intersect([
-    BaseFilterSchema,
+const ArrayValueConditionSchema = v.intersect([
+    BaseConditionSchema,
     v.object({
         operator: v.union([v.literal('in'), v.literal('not_in')]),
         value: v.array(v.union([v.string(), v.number(), v.boolean()])),
     }),
 ]);
 
-const DateValueFilterSchema = v.intersect([
-    BaseFilterSchema,
+const DateValueConditionSchema = v.intersect([
+    BaseConditionSchema,
     v.object({
         operator: v.union([ v.literal('equal'), v.literal('not_equal'), v.literal('gt'), v.literal('gtequal'), v.literal('lt'), v.literal('ltequal'), v.literal('empty'), v.literal('not_empty')]),
         value: v.pipe(v.string(), v.isoDateTime())
     }),
 ]);
 
-const NumericValueFilterSchema = v.intersect([
-	BaseFilterSchema,
+const NumericValueConditionSchema = v.intersect([
+	BaseConditionSchema,
 	v.object({
 		operator: v.union([ v.literal('equal'), v.literal('not_equal'), v.literal('gt'), v.literal('gtequal'), v.literal('lt'), v.literal('ltequal'), v.literal('empty'), v.literal('not_empty')]),
 		value: v.union([v.number(), v.boolean()]),
 	}),
 ]);
 
-const TextValueFilterSchema = v.intersect([
-	BaseFilterSchema,
+const TextValueConditionSchema = v.intersect([
+	BaseConditionSchema,
 	v.object({
 		operator: v.union([ v.literal('equal'), v.literal('not_equal'), v.literal('contains'), v.literal('empty'), v.literal('like'), v.literal('not_empty'), v.literal('starts_with'), v.literal('ends_with')]),
 		value: v.union([v.string(), v.boolean()]),
 	}),
 ]);
 
-export const FilterSchema = v.union([
-    ArrayValueFilterSchema,
-    DateValueFilterSchema,
-    NumericValueFilterSchema,
-	TextValueFilterSchema,
+export const ConditionSchema = v.union([
+    ArrayValueConditionSchema,
+    DateValueConditionSchema,
+    NumericValueConditionSchema,
+	TextValueConditionSchema,
 ]);
 
-export const FilterGroupSchema: v.GenericSchema<FilterGroup> = v.lazy(() =>
+export const ConditionGroupSchema: v.GenericSchema<ConditionGroup> = v.lazy(() =>
     v.object({
         operator: v.union([v.literal('and'), v.literal('or')]),
-        conditions: v.array(v.union([FilterSchema, FilterGroupSchema])),
+        conditions: v.array(v.union([ConditionSchema, ConditionGroupSchema])),
     })
 );
 
 export const SimpleColumnSchema = v.object({
-    field: v.string(),
-    label: v.optional(v.string()),
+    column: v.string(),
+    as: v.optional(v.string()),
 });
 
 export const AggregateColumnSchema = v.object({
     func: v.union([ v.literal('sum'), v.literal('avg'), v.literal('count'), v.literal('min'), v.literal('max')]),
-    field: v.string(),
-    label: v.string(),
+    column: v.string(),
+    as: v.string(),
 });
 
 export const ColumnSchema = v.union([
@@ -68,15 +68,15 @@ export const ColumnSchema = v.union([
     AggregateColumnSchema,
 ]);
 
-export const SortSchema = v.object({
-    field: v.string(),
+export const OrderSchema = v.object({
+    column: v.string(),
     direction: v.union([v.literal('asc'), v.literal('desc')]),
 });
 
-export const QueryOptionsSchema = v.object({
-    filters: v.array(FilterGroupSchema),
-    columns: v.array(ColumnSchema),
-    sorts: v.optional(v.array(SortSchema)),
+export const QuerySchema = v.object({
+    select: v.array(ColumnSchema),
+	where: v.array(ConditionGroupSchema),
+    orderBy: v.optional(v.array(OrderSchema)),
     groupBy: v.optional(v.array(v.string())),
     limit: v.optional(v.pipe(
 		v.number(),
@@ -91,11 +91,11 @@ export const QueryOptionsSchema = v.object({
 	)),
 });
 
-export type FilterGroup = {
+export type ConditionGroup = {
     operator: 'and' | 'or';
-    conditions: (v.InferOutput<typeof FilterSchema> | FilterGroup)[];
+    conditions: (v.InferOutput<typeof ConditionSchema> | ConditionGroup)[];
 };
-export type Filter = v.InferInput<typeof FilterSchema>;
+export type Condition = v.InferInput<typeof ConditionSchema>;
 export type Column = v.InferInput<typeof ColumnSchema>;
-export type Sort = v.InferInput<typeof SortSchema>;
-export type QueryOptions = v.InferInput<typeof QueryOptionsSchema>;
+export type Order = v.InferInput<typeof OrderSchema>;
+export type Query = v.InferInput<typeof QuerySchema>;
